52433aecc08292cd72084ea4043f6940ff273409
simmanasiya <simmanasiya@gmail.com>
2025-06-05 18:38:55 -0500

    Add files via upload

---
+++ catchme-game/Simran_01-11-2022.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnCatchMe = New System.Windows.Forms.Button()
+        Me.tmrGameTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'btnCatchMe
+        '
+        Me.btnCatchMe.Location = New System.Drawing.Point(304, 163)
+        Me.btnCatchMe.Name = "btnCatchMe"
+        Me.btnCatchMe.Size = New System.Drawing.Size(163, 78)
+        Me.btnCatchMe.TabIndex = 0
+        Me.btnCatchMe.Text = "Catch Me!"
+        Me.btnCatchMe.UseVisualStyleBackColor = True
+        '
+        'tmrGameTimer
+        '
+        Me.tmrGameTimer.Enabled = True
+        Me.tmrGameTimer.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnCatchMe)
+        Me.Name = "Form1"
+        Me.Text = "Catch Me"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnCatchMe As Button
+    Friend WithEvents tmrGameTimer As Timer
+End Class
---
+++ catchme-game/Simran_01-11-2022.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub tmrGameTimer_Tick(sender As Object, e As EventArgs) Handles tmrGameTimer.Tick
+        ' Create a Random object
+        Dim rand As New Random
+
+        ' Variables to hold XY coordinates
+        Dim intNewLeft, intNewTop As Integer
+
+        ' Get random XY coordinates.
+        intNewLeft = rand.Next(Me.Width - btnCatchMe.Width)
+        intNewTop = rand.Next(Me.Height - btnCatchMe.Height)
+
+        ' Move the button to the new location.
+        btnCatchMe.Left = intNewLeft
+        btnCatchMe.Top = intNewTop
+    End Sub
+
+    Private Sub btnCatchMe_Click(sender As Object, e As EventArgs) Handles btnCatchMe.Click
+        ' Disable the timer.
+        tmrGameTimer.Enabled = False
+
+        ' Display a message.
+        MessageBox.Show("You win!")
+    End Sub
+End Class
